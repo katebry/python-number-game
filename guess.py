cc21ec014e86acef75dc39a305f920ebbee94ec7
Kate Bryan <katebryan31@gmail.com>
2021-05-08 15:54:24 +0100

    feat: added basic logic to game

--- guess.py
+++ guess.py
@@ -4,5 +4,25 @@ guesses = 0
 
 print('Hello, what is your name?')
 myName = input()
+print('Hi ' + myName +
+      ' nice to meet you, let\'s play a game. I\'m thinking of a number between 1 and 20...')
 
-print(myName)
+number = random.randint(1, 20)
+
+for i in range(5):
+    print('Guess the number')
+    guess = input()
+    guess = int(guess)
+    guesses = guesses + 1
+
+    if guess < number:
+        print('Your guess is too low...')
+    if guess > number:
+        print('Your guess is too high...')
+    if guess == number:
+        break
+
+if guess == number:
+    guesses = str(guesses)
+    print('Good job ' + myName +
+          '! You guessed my number in ' + guesses + ' guesses!')
